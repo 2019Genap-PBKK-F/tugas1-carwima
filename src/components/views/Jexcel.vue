f0263d963c576e3df1bdf2d9fd12128c32a0ff6a
enlighten <grobelwonkerwashere@gmail.com>
2020-02-12 12:49:18 +0700

    first commit

---
+++ src/components/views/Jexcel.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <div id="Jexcel" ref="spreadsheet"></div>
+    <div>
+      <input type="button" value="Add new row" @click="() => spreadsheet.insertRow()" />
+    </div>
+  </div>
+</template>
+
+<script>
+import jexcel from "jexcel";
+import "jexcel/dist/jexcel.css";
+import Axios from 'axios';
+
+var data = [
+  ["Jazz", "Honda", "2019-02-12", "", true, "$ 2.000,00", "#777700"],
+  ["Civic", "Honda", "2018-07-11", "", true, "$ 4.000,01", "#007777"]
+];
+
+var options = {
+  data: data,
+  allowToolbar: true,
+  columns: [
+    { type: "text", title: "Car", width: "120px" },
+    {
+      type: "dropdown",
+      title: "Make",
+      width: "250px",
+      source: ["Alfa Romeo", "Audi", "Bmw"]
+    },
+    { type: "calendar", title: "Available", width: "250px" },
+    { type: "image", title: "Photo", width: "120px" },
+    { type: "checkbox", title: "Stock", width: "80px" },
+    {
+      type: "numeric",
+      title: "Price",
+      width: "100px",
+      mask: "$ #.##,00",
+      decimal: ","
+    },
+    { type: "color", width: "100px", render: "square" }
+  ]
+};
+
+export default {
+  name: "Jexcel",
+  mounted: function() {
+    let spreadsheet = jexcel(this.$el, options);
+    Object.assign(this, { spreadsheet });
+  },
+  methods: {
+    updateonline: function()
+    {
+      Axios.post
+    }
+  }
+};
+</script>
